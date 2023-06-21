982e76b9acca5d1370b88c1ca562393780c9cc31
Nick  Murimi <126444800+NickMurimi@users.noreply.github.com>
2023-06-21 09:12:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Hello. Welcome to your daily affirmation generator.")
+print()
+name = input("What is your name? ")
+if name == "Mukami" or name == "mukami":
+  DOW = input("What is the day of the week?")
+  if DOW == "monday" or DOW == "Monday":
+    print("It is going to be great Monday", name)
+  if DOW == "tuesday" or DOW == "Tuesday":
+    print("What a wonderful Tuesday it is",name)
+  if DOW == "wednesday" or DOW == "Wednesday":
+    print("Happy Hump Day", name)
+  if DOW == "thursday" or DOW == "Thursday":
+    print(name, "yor week is almost over!")
+  if DOW == "friday" or DOW == "Friday":
+    print(name, "its' FRIDAY!")
+elif name == "Nick" or name == "nick":
+  DOW = input("what is the day of the week?")
+  if DOW == "monday" or DOW == "Monday":
+    print("it is going to be great Monday", name)
+  if DOW == "tuesday" or DOW == "Tuesday":
+     print("you look great in that color", name)
+  if DOW == "wednesday" or DOW == "Wednesday":
+    print("You look chipper today", name)
+  if DOW == "thursday" or DOW == "Thursday":
+    print(name, "you are doing great job!")
+  if DOW == "friday" or DOW == "Friday":
+    print(name, "it's FRIDAY!")
+else:
+  print("i do not know your name, but I hope you are having great day!")
+          
+    
+  
+
